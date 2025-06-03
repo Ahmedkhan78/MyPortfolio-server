8b83a1fb010568d9060a7a17fb5f1a7293d03524
AhmedHasan <109160297+Ahmedkhan78@users.noreply.github.com>
2025-06-03 20:41:49 +0530

    Feat: Implemented cors

--- server.js
+++ server.js
@@ -14,7 +14,25 @@ const PORT = process.env.PORT;
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-app.use(cors());
+const allowedOrigins = [
+  "https://ahmeddev-pi.vercel.app",
+  "https://ahmeddev-git-main-ahmeds-projects-0032f4c9.vercel.app",
+  "https://ahmed-lgj4g35fd-ahmeds-projects-0032f4c9.vercel.app",
+  "http://localhost:3000", // for local dev
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed from this origin"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -23,5 +41,5 @@ app.use("/api/contact", contactRoute);
 app.use("/api", resumeRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
