fd0170a6121c669e8635bcf1d5ad4acc97ac5c78
AhmedHasan <109160297+Ahmedkhan78@users.noreply.github.com>
2025-06-03 22:56:20 +0530

    fixes firebase

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,19 +3,9 @@ const fs = require("fs");
 
 const serviceAccountPath = "/etc/secrets/serviceAccountKey.json";
 
-if (!fs.existsSync(serviceAccountPath)) {
-  console.error("❌ serviceaccount.json is missing at", serviceAccountPath);
-  process.exit(1);
-}
-
-let serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
-
-// Ensure the private_key field has newlines properly inserted
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
-
 // Initialize Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccountPath),
 });
 
 const db = admin.firestore();
